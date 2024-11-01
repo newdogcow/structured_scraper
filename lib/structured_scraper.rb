c6a0b30e7a903fe187da26fca9bafa33b3db8b7a
Chris L <chris@dogcow.co.uk>
2024-11-01 16:28:26 +0800

    Empty gem

---
+++ lib/structured_scraper.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "structured_scraper/version"
+
+module StructuredScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/structured_scraper/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module StructuredScraper
+  VERSION = "0.1.0"
+end
